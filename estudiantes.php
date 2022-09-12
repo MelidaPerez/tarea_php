e49ae80f5cee002ef8303c84e61eb9f9c2214839
MelidaPerez <72113969+MelidaPerez@users.noreply.github.com>
2022-09-11 22:03:15 -0600

    Update estudiantes.php

--- estudiantes.php
+++ estudiantes.php
@@ -55,7 +55,7 @@
         </div>
 
         <div class="mb-3">
-            <label for="lbl_tipo_sangre" class="form-label"><b>Puesto</b></label>
+            <label for="lbl_tipo_sangre" class="form-label"><b>Tipo Sangre</b></label>
             <select class="form-select" name="drop_tipo_sangre" id="drop_tipo_sangre">
                 <option value=0> ---- TIPO SANGRE ---- </option>
                 <?php 
